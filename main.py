71103db153e7c819bf1f3cfd311a3ad2748cc0cc
Maximilian Rudolph <lumefx@gmail.com>
2019-03-31 00:57:07 +0100

    Fixed first errors. Basic principle working.

--- main.py
+++ main.py
@@ -13,14 +13,6 @@ class attribute:
     def __init__(self, req):
         self.req = req
 
-def send():
-    print(entryTxt.get())
-
-    w.config(state = NORMAL)
-    w.insert(END, "\n" + entryTxt.get())
-    w.config(state = DISABLED)
-    w.see(INSERT)
-
 def addCommand(cLabel, valFunc = 0, runFunc = 0, usageFunc = 0, descFunc = 0, aBool = "Null"):
     # if (valFunc == 0 or runFunc == 0 or usageFunc == 0 or descFunc == 0):
     #     error(-1)
@@ -40,112 +32,6 @@ def addCommand(cLabel, valFunc = 0, runFunc = 0, usageFunc = 0, descFunc = 0, aB
     c.run = MethodType(runFunc, c)
     commands.append(c)
 
-def helpValidate(c, inp):
-    if (len(inp) > 1):
-        count = 0
-        for i in commands:
-            if (inp[1] != i.label):
-                count += 1
-                if (count == len(commands)): return 1 #Command invalid
-    return 0
-
-def hValidate(c, inp):
-
-    if (len(cmdHistory) < 1):
-        return 4
-
-    if (len(inp) == 2):
-        try:
-            inp[1] = int(inp[1])
-        except ValueError:
-            return 2
-
-        if (inp[1] > len(cmdHistory) or inp[1] < 1):
-            return 2
-
-    return 0
-
-def restartRun(c, inp):
-    scw()
-    scw("Restarting shell")
-    scw()
-    scw("-------------------------------------------------------")
-    return "restart"
-
-def exitRun(c, inp):
-    scw("Goodbye")
-    scw()
-    sys.exit()
-
-def helpRun(c, inp):
-    scw("Type 'help' <command> for commands description.")
-    scw()
-    if (len(inp) > 1):
-        count = 0
-        for i in commands:
-            if (inp[1] == i.label):
-                i.description()
-                break
-    else:
-        for i in range(0, len(commands)):
-            scw(str(i + 1) + ". " + commands[i].label, end = "")
-
-            if (len(commands[i].att) >= 1):
-                scw(" -  ", end = "")
-                commands[i].usage()
-            else:
-                print()
-
-def hRun(c, inp):
-    if (len(inp) == 2):
-        temp = len(cmdHistory) - inp[1]
-    else:
-        temp = len(cmdHistory) - 2
-
-    scw(">> " + cmdHistory[temp])
-    i = input("? ")
-
-    if (i.lower() == ""):
-        main(cmdHistory[temp])
-
-    elif (i.lower() != "n"):
-        error(2, inp[0])
-
-def helpUsage(c):
-    scw("help (<command>)")
-
-def hUsage(c):
-    scw("h (<1-max number>)")
-
-def restartDesc(c):
-    scw("This command restarts this 'shell'. That means it clears the command-history and reloads all of the commands as well as the config file.")
-
-def exitDesc(c):
-    scw("By typing 'exit', the shells main loop will end and the python script will terminate.")
-
-def helpDesc(c):
-    scw("I`m assuming you know what help does, since you just used it. Well it shows you all of the commands you can enter here :)")
-    scw("If you type 'help' <command> it will show you the commands description.")
-    scw("If you just type 'help' it will show all of the commands with their usage.")
-
-def hDesc(c):
-    scw("This command lets you use a command you have recently typed already. It gives you acces to the command history.")
-    scw("You can also type 'h' <number from 1-number of entered commands or maximum length>. This number increases he further back in he history you get.")
-    scw("For the latest command you would type 'h' or 'h 1'. For the secodn latest 'h 2' and so on...")
-    scw("You will get a question mark. You either type enter to execute the command like shown above or type 'n' to abort the process.")
-    scw()
-    scw("You can set the historys maxium length in the config.py file.")
-
-def loadDefaultCmd(defcmdConfig):
-
-    if (defcmdConfig[0] == 0):
-        addCommand("restart", 0, restartRun, 0, restartDesc)
-    if (defcmdConfig[1] == 0):
-        addCommand("exit", 0, exitRun, 0, exitDesc)
-    if (defcmdConfig[2] == 0):
-        addCommand("help", helpValidate, helpRun, helpUsage, helpDesc, False)
-    if (defcmdConfig[3] == 0):
-        addCommand("h", hValidate, hRun, hUsage, hDesc, False)
 
 
 def mainLoad(ld = 0):
@@ -209,41 +95,150 @@ def mainLoad(ld = 0):
     e = Entry(textFrame, textvariable = entryTxt, width = 52)
     e.grid(sticky = "NW")
 
-    b = Button(mw, text = "send", width = 10, command = send)
+    b = Button(mw, text = "send", width = 10, command = main)
     b.grid(row = 3, column = 0, sticky = "E")
 
 
     print()
-    print("done")
-    
-    mw.mainloop()
-    main()
+    scw("done")
+
+def helpValidate(c, inp):
+    if (len(inp) > 1):
+        count = 0
+        for i in commands:
+            if (inp[1] != i.label):
+                count += 1
+                if (count == len(commands)): return 1 #Command invalid
+    return 0
+
+def hValidate(c, inp):
+
+    if (len(cmdHistory) < 1):
+        return 4
+
+    if (len(inp) == 2):
+        try:
+            inp[1] = int(inp[1])
+        except ValueError:
+            return 2
+
+        if (inp[1] > len(cmdHistory) or inp[1] < 1):
+            return 2
+
+    return 0
+
+def restartRun(c, inp):
+    scw()
+    scw("Restarting shell")
+    scw()
+    scw("-------------------------------------------------------")
+    return "restart"
+
+def exitRun(c, inp):
+    scw("Goodbye")
+    scw()
+    sys.exit()
+
+def helpRun(c, inp):
+    print("yee")
+    scw("Type 'help' <command> for commands description.")
+    scw()
+    if (len(inp) > 1):
+        count = 0
+        for i in commands:
+            if (inp[1] == i.label):
+                i.description()
+                break
+    else:
+        for i in range(0, len(commands)):
+            scw(str(i + 1) + ". " + commands[i].label, end = "")
+
+            if (len(commands[i].att) >= 1):
+                scw(" -  ", end = "")
+                commands[i].usage()
+            else:
+                scw()
+
+def hRun(c, inp):
+    if (len(inp) == 2):
+        temp = len(cmdHistory) - inp[1]
+    else:
+        temp = len(cmdHistory) - 2
+
+    scw(">> " + cmdHistory[temp])
+    i = input("? ")
+
+    if (i.lower() == ""):
+        main(cmdHistory[temp])
+
+    elif (i.lower() != "n"):
+        error(2, inp[0])
+
+def helpUsage(c):
+    scw("help (<command>)")
+
+def hUsage(c):
+    scw("h (<1-max number>)")
+
+def restartDesc(c):
+    scw("This command restarts this 'shell'. That means it clears the command-history and reloads all of the commands as well as the config file.")
+
+def exitDesc(c):
+    scw("By typing 'exit', the shells main loop will end and the python script will terminate.")
+
+def helpDesc(c):
+    scw("I`m assuming you know what help does, since you just used it. Well it shows you all of the commands you can enter here :)")
+    scw("If you type 'help' <command> it will show you the commands description.")
+    scw("If you just type 'help' it will show all of the commands with their usage.")
+
+def hDesc(c):
+    scw("This command lets you use a command you have recently typed already. It gives you acces to the command history.")
+    scw("You can also type 'h' <number from 1-number of entered commands or maximum length>. This number increases he further back in he history you get.")
+    scw("For the latest command you would type 'h' or 'h 1'. For the secodn latest 'h 2' and so on...")
+    scw("You will get a question mark. You either type enter to execute the command like shown above or type 'n' to abort the process.")
+    scw()
+    scw("You can set the historys maxium length in the config.py file.")
+
+def loadDefaultCmd(defcmdConfig):
+
+    if (defcmdConfig[0] == 0):
+        addCommand("restart", 0, restartRun, 0, restartDesc)
+    if (defcmdConfig[1] == 0):
+        addCommand("exit", 0, exitRun, 0, exitDesc)
+    if (defcmdConfig[2] == 0):
+        addCommand("help", helpValidate, helpRun, helpUsage, helpDesc, False)
+    if (defcmdConfig[3] == 0):
+        addCommand("h", hValidate, hRun, hUsage, hDesc, False)
+
 
 def main(hCmd = "Null"):
     global cmdHistory
 
-    #Mainloop
-    a = True
-    while(a):
-        #Keep command history at right length
-        if (config.cmdHistoryLen != "n" and len(cmdHistory) > config.cmdHistoryLen):
+    print(entryTxt.get())
+
+    w.config(state = NORMAL)
+    w.insert(END, "\n" + entryTxt.get())
+    w.config(state = DISABLED)
+    w.see(INSERT)
+
+
+    #Keep command history at right length
+    if (config.cmdHistoryLen != "n" and len(cmdHistory) > config.cmdHistoryLen):
             del cmdHistory[0]
 
-        curr = getInp(hCmd) #input gathering
-        hCmd = "Null"
+    curr = getInp(hCmd) #input gathering
+    hCmd = "Null"
 
-        splitArray = split(curr) #input parse
+    splitArray = split(curr) #input parse
 
-        inp = val(splitArray) #input validation
+    inp = val(splitArray) #input validation
 
-        out(inp) #generate output
-
-        mw.mainloop()
+    out(inp) #generate output
 
 def error(errID, inp = 0):
     # There are no switch statements in python! WHY?!?!?
 
-    if (errID > 0):
+    if (errID >= 0):
         print("Error: ", end = " ")
         if (errID == 0):
             print("This input is invalid.")
@@ -262,7 +257,7 @@ def error(errID, inp = 0):
         elif (errID == 6):
             print("Value error. Unable to convert to string.")
 
-        main()
+        mw.mainloop()
 
     else:
         print("CodingError: ", end = " ")
@@ -282,7 +277,7 @@ def getInp(hCmd):
     if (hCmd == "Null"):
         #curr = input(printString + " ")
         curr = entryTxt.get()
-        print()
+        print("curr:", curr)
     else:
         curr = hCmd
 
@@ -299,6 +294,8 @@ def split(curr):
     return splitArray
 
 def val(inp):
+    print("val")
+    print(inp)
 
     #Command validation
     count = 0
@@ -331,10 +328,18 @@ def val(inp):
 
     return inp
 
-def scw(str):
-    w.insert(END, "\n" + str)
+def scw(str = "", end = 0):
+    print("scw")
+    if (end == 0):
+        str = str + '\n'
+
+    w.config(state = NORMAL)
+    w.insert(END, str)
+    w.config(state = DISABLED)
+    print("written")
 
 def out(inp):
+    print("out")
     a = inp[0].run(inp)
 
     temp = inp[0].label
@@ -345,4 +350,6 @@ def out(inp):
     if (a == "restart"):
         mainLoad(load)
 
+
 mainLoad()
+mw.mainloop()
